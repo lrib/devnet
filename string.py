eaeb98ab35bc378ddb74bcaa70d8f4fddf2e2840
lrib <luizcarlos.ribeiro@hotmail.com>
2022-04-27 09:10:37 +0000

    First Upload

---
+++ string.py
@@ -0,0 +1,7 @@
+clube = str (input (" Qual o vosso clube de futebol?"))
+
+idade = int (input (" Quantos anos vc tem?"))
+
+desde = 2022 - idade
+
+print ("Vc é adepto do " + clube + " desde ", desde)
